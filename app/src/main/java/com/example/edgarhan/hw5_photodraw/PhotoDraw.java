ab807b017e98e9a4301a86b8b7007bc6ca7052bf
Edgar Han <edgarh@vt.edu>
2016-11-28 18:41:26 -0500

    1added methods for drawing, haven't tested yet.

--- app/src/main/java/com/example/edgarhan/hw5_photodraw/PhotoDraw.java
+++ app/src/main/java/com/example/edgarhan/hw5_photodraw/PhotoDraw.java
@@ -2,6 +2,9 @@ package com.example.edgarhan.hw5_photodraw;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.BitmapDrawable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,15 @@ public class PhotoDraw extends AppCompatActivity {
 
     MyCanvas myCanvas;
     TouchHandler touchHandler;
+    Bitmap bitmap;
+    Canvas canvas;
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
@@ -27,6 +39,16 @@ public class PhotoDraw extends AppCompatActivity {
         */
 
         takePicture();
+        touchHandler = new TouchHandler(this, canvas, drawPath, drawPaint);
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
 
     private void takePicture() {
@@ -36,16 +58,56 @@ public class PhotoDraw extends AppCompatActivity {
         }
     }
 
+    protected void onDraw() {
+        //draw view
+        canvas.drawBitmap(bitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap thumbnail = (Bitmap) extras.get("data");
+            bitmap = (Bitmap) extras.get("data");
+            canvas = new Canvas(bitmap);
             //bitmap = (Bitmap)extras.get("data");
             //BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
-            myCanvas.setBitmap(thumbnail);
+            myCanvas.setBitmap(bitmap);
             //myCanvas.setBackground(bitmapDrawable);
 
         }
+    }
+
+    /**
+     *
+     * @param x coordinate
+     * @param y coordinate
+     */
+    public void onLongPress(float x, float y) {
+        // add a sticker to the bitmap
+    }
+
+    /**
+     *
+     * @param x coordinate
+     * @param y coordinate
+     */
+    public void onDoubleTap(float x, float y) {
+        // add a sticker to the bitmap
+    }
+
+    public void clickRed() {
+        paintColor = 0xFF000000;
+        drawPaint.setColor(paintColor);
+    }
+
+    public void clickBlue() {
+        paintColor = 0x0000FF00;
+        drawPaint.setColor(paintColor);
+    }
+
+    public void clickGreen() {
+        paintColor = 0x00FF0000;
+        drawPaint.setColor(paintColor);
     }
 }
--- app/src/main/java/com/example/edgarhan/hw5_photodraw/TouchHandler.java
+++ app/src/main/java/com/example/edgarhan/hw5_photodraw/TouchHandler.java
@@ -1,5 +1,8 @@
 package com.example.edgarhan.hw5_photodraw;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v4.view.GestureDetectorCompat;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -11,47 +14,47 @@ import android.view.View;
 
 public class TouchHandler implements View.OnTouchListener {
     MainActivity mainActivity;
+    PhotoDraw photoDraw;
+    Path drawPath;
+    Paint drawPaint;
+    Canvas canvas;
 
     GestureDetectorCompat gestureDetectorCompat;
 
-    public TouchHandler(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
+    //public TouchHandler(MainActivity mainActivity) {
+    public TouchHandler(PhotoDraw pd, Canvas c, Path p, Paint dp) {
+        //this.mainActivity = mainActivity;
+        photoDraw = pd;
+        drawPath = p;
+        canvas = c;
+        drawPaint = dp;
         gestureDetectorCompat = new GestureDetectorCompat(this.mainActivity, new MyGestureListener());
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
         int maskedAction = event.getActionMasked();
         gestureDetectorCompat.onTouchEvent(event);
         switch (maskedAction) {
             case MotionEvent.ACTION_DOWN:
-                // make it upside down
-                //mainActivity.upSideDown();
+                drawPath.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
-                for (int i = 0, size = event.getPointerCount(); i < size; i++) {
-                    int id = event.getPointerId(i);
-                    //mainActivity.addNewPath(id, event.getX(i), event.getY(i));
-                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                for (int i = 0, size = event.getPointerCount(); i < size; i++) {
-                    int id = event.getPointerId(i);
-                    //mainActivity.updatePath(id, event.getX(i), event.getY(i));
-                }
+                drawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
-                // make it rightside up
-                //mainActivity.rightSideUp();
+                canvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
                 break;
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
-                for (int i = 0, size = event.getPointerCount(); i < size; i++) {
-                    int id = event.getPointerId(i);
-                    //mainActivity.removePath(id);
-                }
                 break;
         }
+        photoDraw.onDraw();
         return true;
     }
 
@@ -59,12 +62,14 @@ public class TouchHandler implements View.OnTouchListener {
         @Override
         public void onLongPress(MotionEvent e) {
             //mainActivity.onLongPress();
+            photoDraw.onLongPress(e.getX(), e.getY());
             super.onLongPress(e);
         }
 
         @Override
         public boolean onDoubleTap(MotionEvent e) {
             //mainActivity.onDoubleTap();
+            photoDraw.onDoubleTap(e.getX(), e.getY());
             return super.onDoubleTap(e);
         }
     }
